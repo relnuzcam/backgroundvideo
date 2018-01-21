e862d345537d12ed12c3de0dd757c24332f925b7
Cameron Wilson <cameron@relnuz.com>
2018-01-21 17:20:17 -0500

    updating video settings

--- src/android/VideoOverlay.java
+++ src/android/VideoOverlay.java
@@ -108,8 +108,8 @@ public class VideoOverlay extends ViewGroup implements TextureView.SurfaceTextur
             //Log.d(TAG, sb.toString());
             //profile.videoFrameWidth = sevenTwentyP.width;
             //profile.videoFrameHeight = sevenTwentyP.height;
-            profile.videoFrameWidth = 480;
-            profile.videoFrameHeight = 720;
+            profile.videoFrameWidth = 720;
+            profile.videoFrameHeight = 480;
             profile.videoFrameRate = 24;
             profile.videoBitRate = 2000000;
 
